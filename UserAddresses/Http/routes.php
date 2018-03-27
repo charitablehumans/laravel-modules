1f2903c09cc02f78cb8505b71ee4d3dfccfc276f
Jovi <redzjovi@gmail.com>
2018-03-27 13:26:09 +0700

    Update #modules, #user_addresses.
    - Update user addresses module.

--- UserAddresses/Http/routes.php
+++ UserAddresses/Http/routes.php
@@ -6,8 +6,10 @@ Route::group(['middleware' => ['authApi']], function () {
 });
 
 Route::group(['middleware' => ['web']], function () {
-    Route::group(['middleware' => ['permission:backend user addresses']], function () {
-        Route::resource('backend/user-addresses', '\Modules\UserAddresses\Http\Controllers\Backend\UserAddressesController', ['as' => 'backend']);
-        Route::get('backend/user-addresses/{id}/delete', ['as' => 'backend.user-addresses.delete', 'uses' => '\Modules\UserAddresses\Http\Controllers\Backend\UserAddressesController@delete']);
+    Route::group(['middleware' => ['auth']], function () {
+        Route::group(['middleware' => ['permission:backend user addresses']], function () {
+            Route::resource('backend/user-addresses', '\Modules\UserAddresses\Http\Controllers\Backend\UserAddressesController', ['as' => 'backend']);
+            Route::get('backend/user-addresses/{id}/delete', ['as' => 'backend.user-addresses.delete', 'uses' => '\Modules\UserAddresses\Http\Controllers\Backend\UserAddressesController@delete']);
+        });
     });
 });
