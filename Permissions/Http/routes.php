d8e390250d96b850e54b61e81631583ccc76e7aa
Jovi <redzjovi@gmail.com>
2018-03-27 07:48:00 +0700

    Update #modules, #routes.
    - Comment out the routes.

--- Permissions/Http/routes.php
+++ Permissions/Http/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['middleware' => 'web', 'prefix' => 'permissions', 'namespace' => 'Modules\Permissions\Http\Controllers'], function()
-{
-    Route::get('/', 'PermissionsController@index');
-});
+// Route::group(['middleware' => 'web', 'prefix' => 'permissions', 'namespace' => 'Modules\Permissions\Http\Controllers'], function()
+// {
+//     Route::get('/', 'PermissionsController@index');
+// });
