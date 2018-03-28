859322c6ea09e1419de3f8b73bdb5de7bb5e30b1
Jovi <redzjovi@gmail.com>
2018-03-28 23:32:48 +0700

    Update #modules, #users.
    - Add AuthApi middleware.

---
+++ Users/Http/Middleware/AuthApi.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Modules\Users\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Modules\Users\Models\Users;
+
+class AuthApi
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($user = Users::where('access_token', $request->header('Access-Token'))->first()) {
+            Auth::login($user);
+        } else {
+            return response()->json(['message' => trans('auth.failed')], Response::HTTP_UNAUTHORIZED);
+        }
+
+        return $next($request);
+    }
+}
