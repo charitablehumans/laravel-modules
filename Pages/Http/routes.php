e06ca87aa3705a5161574e1e921740b14cf5bf35
Jovi <redzjovi@gmail.com>
2018-04-22 19:20:46 +0700

    #pages, update.
    Update paramater page to id, in route resource.

--- Pages/Http/routes.php
+++ Pages/Http/routes.php
@@ -3,7 +3,7 @@
 Route::group(['middleware' => ['web']], function () {
     Route::group(['middleware' => ['auth']], function () {
         Route::group(['middleware' => ['permission:backend pages']], function () {
-            Route::resource('backend/pages', '\Modules\Pages\Http\Controllers\Backend\PagesController', ['as' => 'backend']);
+            Route::resource('backend/pages', '\Modules\Pages\Http\Controllers\Backend\PagesController', ['as' => 'backend', 'parameters' => ['pages' => 'id']]);
             Route::get('backend/pages/{id}/delete', ['as' => 'backend.pages.delete', 'uses' => '\Modules\Pages\Http\Controllers\Backend\PagesController@delete']);
             Route::get('backend/pages/{id}/trash', ['as' => 'backend.pages.trash', 'uses' => '\Modules\Pages\Http\Controllers\Backend\PagesController@trash']);
         });
